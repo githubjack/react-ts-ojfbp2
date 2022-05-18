873845e0694f524599f0989c0830edc4c089a92c
jack maynard <jack.ui.maynard@gmail.com>
2022-05-18 16:42:37 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import ProductCustomizer from './App';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('ProductCustomizer');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <ProductCustomizer />
   </StrictMode>
 );
