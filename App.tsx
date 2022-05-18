18c60d2238e9ff6f5029406ffb49a7d69c2c0c89
githubjack <jack.ui.maynard@gmail.com>
2022-05-18 14:34:54 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './style.css';
+
+export function ProductCustomizer() {
+  <div className="customizer">
+    Product customer will go here
+  </div>
+}
+export default function App() {
+  
+  return (
+    <div>
+      <div className="header">
+      <h1>H+ Basketball Sneakers</h1>
+      <p>Run faster, jump higher.</p>
+      </div>
+      <div className="main">
+        
+      </div>
+    </div>
+  );
+}
