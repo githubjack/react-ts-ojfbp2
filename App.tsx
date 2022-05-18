07847a022e4d7dccd0b4ae37292f37551448df09
githubjack <jack.ui.maynard@gmail.com>
2022-05-18 18:01:17 +0100

    2 files modified

--- App.tsx
+++ App.tsx
@@ -2,11 +2,21 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-
+export function ProductImage(props) {
+  return (
+        <img 
+          src="https://cdn.jsdelivr.net/gh/githubjack/react-ts-ojfbp2@master/assets/red.jpg" 
+          alt="Product image" />
+          );
+      }
+  
 export default function ProductCustomizer() {
   return (
-    <div>
-      Hello
+    <div className="customizer">
+      Hello customizer 
+      <div className="product-image">
+        <ProductImage />
+      </div>
     </div>
   )
 }
