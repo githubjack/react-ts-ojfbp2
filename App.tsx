bfe73d41fc37758e3c8cd61d2b0f4bb5249094cb
githubjack <jack.ui.maynard@gmail.com>
2022-05-19 13:16:05 +0100

    3 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import './inventory';
 import './style.css';
 
 export function ProductImage(props) {
@@ -17,6 +18,25 @@ export default function ProductCustomizer() {
       <div className="product-image">
         <ProductImage />
       </div>
+      <div className="selectors">
+							<div className="field-group">
+								<label for="size-options">Size:</label>
+								<select name="sizeOptions" id="size-options">
+									<option>7</option>
+									<option>7.5</option>
+									<option>8</option>
+									<option>8.5</option>
+									<option>9</option>
+									<option>9.5</option>
+									<option>10</option>
+									<option>10.5</option>
+									<option>11</option>
+									<option>11.5</option>
+									<option>12</option>
+									<option>12.5</option>
+								</select>
+							</div>
+						</div>
     </div>
   )
 }
